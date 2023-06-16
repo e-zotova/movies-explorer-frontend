c4f0b552b9b7b66395dfb61fb285d5a610b51659
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-16 15:18:28 +0300

    feat: header for landing and movies

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import { useState } from "react";
 import headerLogo from "../../images/header-logo.svg";
+import profileLogo from "../../images/profile-icon.svg";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 
 function Header() {
+  const [loggedIn, setLoggedIn] = useState(true);
+
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
@@ -13,37 +16,57 @@ function Header() {
   }
 
   return (
-    <div className="header">
+    <div className={`header ${loggedIn ? "" : "header__landing"}`}>
       <img
         className="header__logo"
         src={headerLogo}
-        alt="Логотип Место Россия"
+        alt="Логотип дипломного проекта"
       />
       {pathname === "/" && (
-        <div className="header__links">
-          <Link
-            to="/signup"
-            type="button"
-            className="header__signup"
-          >
+        <div className="header__link">
+          <Link to="/signup" type="button" className="button header__signup">
             Регистрация
           </Link>
-          <Link
-            to="/signin"
-            type="button"
-            className="header__signin"
-          >
+          <Link to="/signin" type="button" className="button header__signin">
             Войти
           </Link>
         </div>
       )}
-      {pathname === "/sign-in" && (
-        <Link to="/sign-up" className="header__link" type="button">
-        </Link>
+      {(pathname === "/movies" || pathname === "/saved-movies" || pathname === "/profile") && (
+        <div className="header__links">
+          <div>
+            <Link type="button" className="button header__movies">
+              Фильмы
+            </Link>
+            <Link
+              to="/saved-movies"
+              type="button"
+              className="button header__saved-movies"
+            >
+              Сохраненные фильмы
+            </Link>
+          </div>
+          <div className="header__profile-button">
+            <img
+              className="header__profile-logo"
+              src={profileLogo}
+              alt="Иконка профиля"
+            />
+            <Link
+              to="/profile"
+              type="button"
+              className="button header__profile-text"
+            >
+              Аккаунт
+            </Link>
+          </div>
+        </div>
       )}
-      {pathname === "/sign-up" && (
-        <Link to="/sign-in" className="header__link" type="button">
-        </Link>
+      {pathname === "/signin" && (
+        <Link to="/signup" className="header__link" type="button"></Link>
+      )}
+      {pathname === "/signup" && (
+        <Link to="/signin" className="header__link" type="button"></Link>
       )}
     </div>
   );
