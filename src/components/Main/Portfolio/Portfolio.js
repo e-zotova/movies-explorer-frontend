9aefddf257816c9cbc39fefe91f537a0465c47e3
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-19 15:32:47 +0300

    feat: footer component

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -3,7 +3,37 @@ import React from "react";
 function Portfolio() {
   return (
     <div className="portfolio">
-
+      <h2 className="portfolio__header">Портфолио</h2>
+      <div className="portfolio__container">
+        <a
+          href="https://github.com/e-zotova/how-to-learn"
+          target="blank"
+          className="button portfolio__project"
+        >
+          <div className="portfolio__site">Статичный сайт</div>
+          <div className="portfolio__link"></div>
+        </a>
+        <div className="dividing-line portfolio__dividing-line"></div>
+        <a
+          href="https://github.com/e-zotova/russian-travel"
+          target="blank"
+          className="button portfolio__project"
+        >
+          <div className="portfolio__site">Адаптивный сайт</div>
+          <div className="portfolio__link"></div>
+        </a>
+        <div className="dividing-line portfolio__dividing-line"></div>
+        <a
+          href="https://github.com/e-zotova/react-mesto-api-full-gha"
+          target="blank"
+          className="button portfolio__project"
+        >
+          <div className="portfolio__site">
+            Одностраничное приложение
+          </div>
+          <div className="portfolio__link"></div>
+        </a>
+      </div>
     </div>
   );
 }
