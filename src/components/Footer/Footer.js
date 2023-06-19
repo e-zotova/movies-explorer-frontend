3037bd2d4406c414c660db138d5158f4e63179ee
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-19 16:26:51 +0300

    feat: updated footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,13 +8,13 @@ function Footer() {
       </p>
       <div className="dividing-line footer__dividing-line"></div>
       <div className="footer__info">
-        <div>&copy; 2023</div>
+        <div className="footer__link">&copy; 2023</div>
         <div className="footer__links">
           <a
             href="https://practicum.yandex.ru/"
             target="_blank"
             rel="noreferrer"
-            className="button footer__link"
+            className="button footer__link footer__praktikum-link"
           >
             Яндекс.Практикум
           </a>
