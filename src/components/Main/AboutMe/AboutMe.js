c8819431c290ff7384ce8524a6c0c9c441726ebc
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-19 13:41:03 +0300

    feat: aboutMe component

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -1,9 +1,35 @@
 import React from "react";
+import profilePhoto from "../../../images/photo.jpeg";
 
 function AboutMe() {
   return (
     <div className="about-me">
-
+      <h2 className="about-me__header">Студент</h2>
+      <div className="dividing-line"></div>
+      <div className="about-me__container">
+        <div className="about-me__description">
+          <h3 className="about-me__subheader">Елена</h3>
+          <p className="about-me__title">Фронтенд-разработчица</p>
+          <p className="about-me__text">
+            Живу в Санкт-Петербурге с 2017 года. Работала тестировщиком сначала
+            мобильных, а затем веб-приложений. С 2019 по 2022 работала в
+            г.Монреаль (Канада). Вернулась, чтобы начать заново и научиться
+            веб-разработке.
+          </p>
+          <a
+            href="https://github.com/e-zotova/"
+            target="blank"
+            className="button about-me__github"
+          >
+            Github
+          </a>
+        </div>
+        <img
+          className="about-me__photo"
+          src={profilePhoto}
+          alt="Фото профиля"
+        ></img>
+      </div>
     </div>
   );
 }
