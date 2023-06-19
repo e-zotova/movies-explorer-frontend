f555056a3269771c7e5dc49aeb3208497c58dd52
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-19 12:30:33 +0300

    feat: techs component

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -3,7 +3,24 @@ import React from "react";
 function Techs() {
   return (
     <div className="techs">
-
+      <h2 className="techs__header">Технологии</h2>
+      <div className="dividing-line"></div>
+      <div className="techs__description">
+        <h3 className="techs__subheader">7 технологий</h3>
+        <p className="techs__text">
+          На курсе веб-разработки мы освоили технологии, которые применили в
+          дипломном проекте.
+        </p>
+      </div>
+      <div className="techs__stack">
+        <div className="techs__stack-element">HTML</div>
+        <div className="techs__stack-element">CSS</div>
+        <div className="techs__stack-element">JS</div>
+        <div className="techs__stack-element">React</div>
+        <div className="techs__stack-element">Git</div>
+        <div className="techs__stack-element">Express.js</div>
+        <div className="techs__stack-element">mongoDB</div>
+      </div>
     </div>
   );
 }
