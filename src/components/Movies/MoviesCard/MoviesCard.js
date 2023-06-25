b752ddf385ea89334d726e53b1b702011d69e428
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-25 15:53:20 +0400

    feat: like/dislike movies on movies page

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,13 +1,17 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
+import { useLocation } from "react-router-dom";
 import { CurrentUserContext } from "../../../contexts/CurrentUserContext";
+import saveButton from "../../../images/save-inactive.svg";
+import saveActiveButton from "../../../images/save-active.svg";
 
 function MoviesCard({ movie, onMovieSave }) {
   const currentUser = useContext(CurrentUserContext);
+  const { pathname } = useLocation();
 
-  const movieSaveButtonClassName = `movies-card__save-movie ${"movies-card__save-button_active"}`;
+  const [isSaved, setIsSaved] = useState(false);
 
-  function handleSaveClick() {
-    onMovieSave(movie);
+  function onMovieSave() {
+    setIsSaved(!isSaved);
   }
 
   return (
@@ -16,12 +20,31 @@ function MoviesCard({ movie, onMovieSave }) {
       <div className=" button movies-card__description">
         <div className="movies-card__item">
           <h2 className="movies-card__name">{movie.name}</h2>
-          <button
-            type="button"
-            aria-label="Добавить в избранное"
-            className={`button movies-card__save-movie ${movieSaveButtonClassName}`}
-            onClick={handleSaveClick}
-          />
+          {pathname === "/movies" && (
+            <button
+              type="button"
+              aria-label="Добавить в избранное"
+              className={
+                isSaved
+                  ? "button movies-card__save-button_saved"
+                  : "button movies-card__save-button"
+              }
+              onClick={onMovieSave}
+            >
+              <img
+                src={isSaved ? saveActiveButton : saveButton}
+                alt="Добавить в избранное"
+              />
+            </button>
+          )}
+          {pathname === "/saved-movies" && (
+            <button
+              type="button"
+              aria-label="Удалить из избранного"
+              className="button movies-card__save-button"
+              onClick={onMovieSave}
+            />
+          )}
         </div>
         <div className="movies-card__duration">{movie.duration}</div>
       </div>
