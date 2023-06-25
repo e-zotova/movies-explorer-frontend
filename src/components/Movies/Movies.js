fc205875b9c8cbaa74f10c474a0c0222acc99a44
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-25 12:19:24 +0400

    feat: added preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import SearchForm from "../Movies/SearchForm/SearchForm.js";
 import Preloader from "../Movies/Preloader/Preloader.js";
 import MoviesCardList from "../Movies/MoviesCardList/MoviesCardList.js";
 import { moviesList } from "../../constants/movies.js";
 
 function Movies() {
+  const [preloader, setPreloader] = useState(false);
+
   return (
     <section className="movies">
       <SearchForm />
-      <Preloader />
-      <MoviesCardList movies={moviesList} />
+      {preloader && <Preloader />}
+      {!preloader && <MoviesCardList movies={moviesList} />}
       <div className="movies__more">
         <div className="movies__more-button">Ещё</div>
       </div>
--- src/components/Movies/Preloader/Preloader.js
+++ src/components/Movies/Preloader/Preloader.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./Preloader.css";
 
-function Preloader() {
+const Preloader = () => {
   return (
     <div className="preloader">
-
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
     </div>
   );
-}
+};
 
 export default Preloader;
