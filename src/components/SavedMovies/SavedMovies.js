15a893ae766976dbf9bf2f8cbeb7cbf4ae6f2d8a
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-25 12:43:17 +0400

    feat: 768 saved-movies pixel perfect

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import { useState } from "react";
 import SearchForm from '../Movies/SearchForm/SearchForm.js';
 import Preloader from '../Movies/Preloader/Preloader.js';
 import MoviesCardList from '../Movies/MoviesCardList/MoviesCardList.js';
 import { savedMoviesList } from "../../constants/movies.js";
 
 function SavedMovies() {
+  const [preloader, setPreloader] = useState(false);
 
   return (
     <div className="saved-movies">
       <SearchForm />
-      <Preloader />
-      <MoviesCardList movies={savedMoviesList}/>
+      {preloader && <Preloader />}
+      {!preloader && <MoviesCardList movies={savedMoviesList}/>}
       <div className="saved-movies__divider"></div>
     </div>
   );
