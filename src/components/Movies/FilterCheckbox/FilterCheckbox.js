d4b771c0a85a7ab5251b8211d7a651d9016cea45
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-25 18:08:01 +0400

    fix: console error, preloader

--- src/components/Movies/FilterCheckbox/FilterCheckbox.js
+++ src/components/Movies/FilterCheckbox/FilterCheckbox.js
@@ -3,9 +3,9 @@ import React from "react";
 function FilterCheckbox() {
   return (
     <div className="filter-checkbox">
-      <label class="filter-checkbox__switch">
+      <label className="filter-checkbox__switch">
         <input className="filter-checkbox__input" type="checkbox"></input>
-        <span class="button filter-checkbox__slider"></span>
+        <span className="button filter-checkbox__slider"></span>
       </label>
       <p className="filter-checkbox__text">Короткометражки</p>
     </div>
