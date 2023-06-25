7000ff6c24d9902da53a5e0173f8c72fccaa20ec
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-25 13:47:38 +0400

    refactor: changed filterCheckbox

--- src/components/Movies/FilterCheckbox/FilterCheckbox.js
+++ src/components/Movies/FilterCheckbox/FilterCheckbox.js
@@ -3,7 +3,10 @@ import React from "react";
 function FilterCheckbox() {
   return (
     <div className="filter-checkbox">
-      <button className="filter-checkbox__switcher"></button>
+      <label class="filter-checkbox__switch">
+        <input className="filter-checkbox__input" type="checkbox"></input>
+        <span class="button filter-checkbox__slider"></span>
+      </label>
       <p className="filter-checkbox__text">Короткометражки</p>
     </div>
   );
