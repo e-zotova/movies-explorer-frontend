2d9a635ee714df008b423f5187d3a98467ea6ee1
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-27 14:18:24 +0400

    feat: 1280 profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,22 +1,42 @@
-import React from "react";
+import { useContext } from 'react';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext.js';
 
 function Profile() {
+  const { name, email } = useContext(CurrentUserContext);
+
   return (
     <section className="profile">
       <div className="profile__info">
-        <h1 className="profile__header">Привет, Елена!</h1>
-        <div className="profile__name">
-          <p>Имя</p>
-          <p>Елена</p>
-        </div>
-        <div className="profile__email">
-          <p>Email</p>
-          <p>pochta.yandex.ru</p>
-        </div>
+        <h1 className="profile__header">Привет, {name}!</h1>
+        <label className="profile__name-container">
+          <span className="profile__text profile__name-label">Имя</span>
+          <input
+            type="text"
+            className="profile__text profile__input"
+            required={true}
+            value={name}
+            minLength={2}
+            maxLength={30}
+          ></input>
+        </label>
+        <div className="dividing-line profile__dividing-line"></div>
+        <label className="profile__email-container">
+          <span className="profile__text profile__email-label">Email</span>
+          <input
+            type="email"
+            className="profile__text profile__input"
+            value={email}
+            required={true}
+          ></input>
+        </label>
       </div>
       <div className="profile__buttons">
-        <button className="button profile__button profile__edit-button">Редактировать</button>
-        <button className="button profile__button profile__signout-button">Выйти из аккаунта</button>
+        <button className="button profile__button profile__edit-button">
+          Редактировать
+        </button>
+        <button className="button profile__button profile__signout-button">
+          Выйти из аккаунта
+        </button>
       </div>
     </section>
   );
