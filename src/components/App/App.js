44e48f9ed4a0dd0b58d09818b5e9c9a921f7dfe2
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-27 14:18:43 +0400

    feat: added current user context

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "../Header/Header.js";
@@ -9,10 +10,17 @@ import Profile from "../Profile/Profile.js";
 import Login from "../Login/Login.js";
 import Register from "../Register/Register.js";
 import PageNotFound from "../PageNotFound/PageNotFound.js";
+import { CurrentUserContext } from '../../contexts/CurrentUserContext.js';
 
 function App() {
+  const [ currentUser, setCurrentUser ] = useState({
+    name: 'Елена',
+    email: 'pochta@yandex.ru',
+  });
+
   return (
     <div className="content">
+      <CurrentUserContext.Provider value={currentUser}>
       <Header className="header" />
       <Routes>
         <Route path="/" element={<Main />} />
@@ -24,6 +32,7 @@ function App() {
         <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer className="footer" />
+      </CurrentUserContext.Provider>
     </div>
   );
 }
