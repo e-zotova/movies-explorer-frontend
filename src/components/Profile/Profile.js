1bc805b0ed1e597dd0ffe1ec1902dedbf8649602
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-27 13:43:40 +0400

    feat: profile compopent

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 function Profile() {
-
   return (
-    <div className="profile">
-
-    </div>
+    <section className="profile">
+      <div className="profile__info">
+        <h1 className="profile__header">Привет, Елена!</h1>
+        <div className="profile__name">
+          <p>Имя</p>
+          <p>Елена</p>
+        </div>
+        <div className="profile__email">
+          <p>Email</p>
+          <p>pochta.yandex.ru</p>
+        </div>
+      </div>
+      <div className="profile__buttons">
+        <button className="button profile__button profile__edit-button">Редактировать</button>
+        <button className="button profile__button profile__signout-button">Выйти из аккаунта</button>
+      </div>
+    </section>
   );
 }
 
