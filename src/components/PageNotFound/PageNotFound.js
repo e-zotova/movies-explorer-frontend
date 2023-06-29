cd01b7de2dd5dafe2146e69a689ec49aa1114193
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-29 13:15:31 +0400

    feat: 404 pixel perfect 768 320

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function PageNotFound() {
-
   return (
     <div className="page-not-found">
-
+      <h1 className="page-not-found__error-code">404</h1>
+      <h2 className="page-not-found__error-name">Страница не найдена</h2>
+      <a className="button page-not-found__back-button" href="/">
+        Назад
+      </a>
     </div>
   );
 }
