a7493655afa4411a87cf69aa7b2bac87d0d25104
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-29 19:11:23 +0400

    refactor: submit profile, remove error message

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,12 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
 
-function Profile( {onUpdateProfile} ) {
+function Profile({ onUpdateProfile }) {
   const navigate = useNavigate();
   const { name, email } = useContext(CurrentUserContext);
-  const [formValue, setFormValue] = useState({name, email});
+  const [formValue, setFormValue] = useState({ name, email });
 
-  const [errorMessage, setErrorMessage] = useState("");
   const [isProfileEditing, setisProfileEditing] = useState(false);
 
   function changeProfileEditing(state) {
@@ -29,22 +28,11 @@ function Profile( {onUpdateProfile} ) {
     changeProfileEditing(false);
   }
 
-  function saveProfile() {
-    changeProfileEditing(true);
-  }
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { name, email } = formValue;
-    const emptyInputError = "Имя пользователя и почта должны быть заполнены";
-
-    if (!name || !email ) {
-      setErrorMessage(emptyInputError);
-
-    } else {
-      onUpdateProfile(name, email)
-    }
-  }
+    changeProfileEditing(true);
+    onUpdateProfile(name, email);
+  };
 
   function onSignOut() {
     localStorage.removeItem("jwt");
@@ -54,63 +42,60 @@ function Profile( {onUpdateProfile} ) {
 
   return (
     <section className="profile">
-      <form className="profile__info" onSubmit={handleSubmit}>
+      <div className="profile__info">
         <h1 className="profile__header">Привет, {name}!</h1>
-        <label className="profile__name-container">
-          <span className="profile__text profile__name-label">Имя</span>
-          <input
-            id="name"
-            type="text"
-            className="profile__text profile__input"
-            required={true}
-            defaultValue={name}
-            minLength={2}
-            maxLength={30}
-            onChange={handleChange}
-            disabled
-          ></input>
-        </label>
-        <div className="dividing-line profile__dividing-line"></div>
-        <label className="profile__email-container">
-          <span className="profile__text profile__email-label">Email</span>
-          <input
-            id="email"
-            type="email"
-            className="profile__text profile__input"
-            defaultValue={email}
-            required={true}
-            onChange={handleChange}
-            disabled
-          ></input>
-        </label>
-        <span className="profile__error">{errorMessage}</span>
-        <div className="profile__buttons">
-        {isProfileEditing ? (
-          <button
-            type="submit"
-            className="button profile__save-button"
-            onClick={saveProfile}
-          >
-            Сохранить
-          </button>
-        ) : (
-          <div className="profile__edit">
-            <button
-              className="button profile__button profile__edit-button"
-              onClick={editProfile}
-            >
-              Редактировать
-            </button>
-            <button
-              className="button profile__button profile__signout-button"
-              onClick={onSignOut}
-            >
-              Выйти из аккаунта
-            </button>
+        <form onSubmit={handleSubmit}>
+          <label className="profile__name-container">
+            <span className="profile__text profile__name-label">Имя</span>
+            <input
+              id="name"
+              type="text"
+              className="profile__text profile__input"
+              required={true}
+              defaultValue={name}
+              minLength={2}
+              maxLength={30}
+              onChange={handleChange}
+              disabled
+            ></input>
+          </label>
+          <div className="dividing-line profile__dividing-line"></div>
+          <label className="profile__email-container">
+            <span className="profile__text profile__email-label">Email</span>
+            <input
+              id="email"
+              type="email"
+              className="profile__text profile__input"
+              defaultValue={email}
+              required={true}
+              onChange={handleChange}
+              disabled
+            ></input>
+          </label>
+          <div className="profile__buttons">
+            {isProfileEditing ? (
+              <button type="submit" className="button profile__save-button">
+                Сохранить
+              </button>
+            ) : (
+              <div className="profile__edit">
+                <button
+                  className="button profile__button profile__edit-button"
+                  onClick={editProfile}
+                >
+                  Редактировать
+                </button>
+                <button
+                  className="button profile__button profile__signout-button"
+                  onClick={onSignOut}
+                >
+                  Выйти из аккаунта
+                </button>
+              </div>
+            )}
           </div>
-        )}
-        </div>
-      </form>
+        </form>
+      </div>
     </section>
   );
 }
