de588fa4af53f5e06d2ddac8fe27d4d4ddb8819b
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-29 12:37:26 +0400

    feat: add register component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "../Header/Header.js";
@@ -10,32 +10,39 @@ import Profile from "../Profile/Profile.js";
 import Login from "../Login/Login.js";
 import Register from "../Register/Register.js";
 import PageNotFound from "../PageNotFound/PageNotFound.js";
-import { CurrentUserContext } from '../../contexts/CurrentUserContext.js';
+import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
 
 function App() {
-  const [ currentUser, setCurrentUser ] = useState({
-    name: 'Елена',
-    email: 'pochta@yandex.ru',
+  const [currentUser, setCurrentUser] = useState({
+    name: "Елена",
+    email: "pochta@yandex.ru",
   });
 
   function onLogin(email, password) {
     // call api
   }
 
+  function onRegister(email, password) {
+    // call api
+  }
+
   return (
     <div className="content">
       <CurrentUserContext.Provider value={currentUser}>
-      <Header className="header" />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/saved-movies" element={<SavedMovies />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signin" element={<Login onLogin={onLogin}/>} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-      <Footer className="footer" />
+        <Header className="header" />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/saved-movies" element={<SavedMovies />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Login onLogin={onLogin} />} />
+          <Route
+            path="/signup"
+            element={<Register onRegister={onRegister} />}
+          />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer className="footer" />
       </CurrentUserContext.Provider>
     </div>
   );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function Register() {
+function Register({ onRegister }) {
+  const [formValue, setFormValue] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password } = formValue;
+    const emptyInputError = "Имя пользователя, почта и пароль должны быть заполнены";
+
+    if (!name || !email || !password) {
+      setErrorMessage(emptyInputError);
+
+    } else {
+      onRegister(email, password);
+    }
+  };
 
   return (
     <div className="register">
-      <h1 className='register__header'>Добро пожаловать!</h1>
+      <h1 className="register__header">Добро пожаловать!</h1>
+      <form className={`login__form`} onSubmit={onSubmit}>
+      <span className="login__label">Имя</span>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          className="input login__input"
+          value={formValue.name}
+          onChange={handleChange}
+        />
+        <span className="login__label">E-mail</span>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          className="input login__input"
+          value={formValue.email}
+          onChange={handleChange}
+        />
+        <span className="login__label">Пароль</span>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          className="input login__input"
+          value={formValue.username}
+          onChange={handleChange}
+        />
+        <span className="login__error">{errorMessage}</span>
+        <button
+          type="submit"
+          className="button register__submit-button"
+        >
+          Зарегистрироваться
+        </button>
+        <span className="login__switch-label">
+          Уже зарегистрированы?
+          <a className="button login__switch-button" href="/signin">
+            Войти
+          </a>
+        </span>
+      </form>
     </div>
   );
 }
