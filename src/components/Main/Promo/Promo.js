6be150f3d99fd2bc44c762054e6320322f8c166d
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-06-30 13:28:23 +0400

    fix: pixel perfect landing page

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -3,12 +3,9 @@ import React from "react";
 function Promo() {
   return (
     <div className="promo">
-      <div className="promo__overlay">
-        <h1 className="promo__title">
-          Учебный&nbsp;проект студента
-          факультета Веб&#8209;разработки.
-        </h1>
-      </div>
+      <h1 className="promo__title">
+        Учебный&nbsp;проект студента факультета Веб&#8209;разработки.
+      </h1>
     </div>
   );
 }
