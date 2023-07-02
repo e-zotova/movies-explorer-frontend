11af4badf9930e4cbda41e7bae30af554eec1823
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-07-02 23:10:01 +0400

    fix: dividing-line for portfolio

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -5,7 +5,7 @@ function Portfolio() {
     <section className="portfolio">
       <h2 className="portfolio__header">Портфолио</h2>
       <ul className="portfolio__list">
-        <li>
+        <li className="portfolio__link">
           <a
             href="https://github.com/e-zotova/how-to-learn"
             target="_blank"
@@ -13,10 +13,10 @@ function Portfolio() {
             className="button portfolio__site"
           >
             Статичный сайт
-            <div className="portfolio__link"></div>
+            <div className="portfolio__icon"></div>
           </a>
         </li>
-        <li>
+        <li className="portfolio__link">
           <a
             href="https://github.com/e-zotova/russian-travel"
             target="_blank"
@@ -24,10 +24,10 @@ function Portfolio() {
             className="button portfolio__site"
           >
             Адаптивный сайт
-            <div className="portfolio__link"></div>
+            <div className="portfolio__icon"></div>
           </a>
         </li>
-        <li>
+        <li className="portfolio__link">
           <a
             href="https://github.com/e-zotova/react-mesto-api-full-gha"
             target="_blank"
@@ -35,7 +35,7 @@ function Portfolio() {
             className="button portfolio__site"
           >
             Одностраничное приложение
-            <div className="portfolio__link"></div>
+            <div className="portfolio__icon"></div>
           </a>
         </li>
       </ul>
