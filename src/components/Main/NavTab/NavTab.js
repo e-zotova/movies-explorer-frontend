194ec5d04e9e4678ed69bf964bdb9f4b861547b9
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-07-02 17:31:53 +0400

    review: add list and nav to navtab

--- src/components/Main/NavTab/NavTab.js
+++ src/components/Main/NavTab/NavTab.js
@@ -2,11 +2,25 @@ import React from "react";
 
 function NavTab() {
   return (
-    <div className="navtab">
-      <a href="#about-project" className="button navtab__link">О проекте</a>
-      <a href="#techs" className="button navtab__link">Технологии</a>
-      <a href="#about-me" className="button navtab__link">Студент</a>
-    </div>
+    <nav className="navtab">
+      <ul className="navtab__list">
+        <li className="navtab__list-item">
+          <a href="#about-project" className="button navtab__link">
+            О&nbsp;проекте
+          </a>
+        </li>
+        <li className="navtab__list-item">
+          <a href="#techs" className="button navtab__link">
+            Технологии
+          </a>
+        </li>
+        <li className="navtab__list-item">
+          <a href="#about-me" className="button navtab__link">
+            Студент
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
