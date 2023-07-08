8d9297ef6a5ee83ae9260f4b3e1b063750975410
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-07-08 19:18:01 +0400

    refactor: remove text constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,7 @@
 const BASE_MAIN_URL = "http://localhost:3001";
 const BASE_MOVIES_URL = "https://api.nomoreparties.co"
 
-const profileUpdateSuccess = "Профиль успешно обновлен.";
-const profileUpdateFail = "При обновлении профиля произошла ошибка.";
-
 module.exports = {
   BASE_MAIN_URL,
   BASE_MOVIES_URL,
-  profileUpdateSuccess,
-  profileUpdateFail
 }
