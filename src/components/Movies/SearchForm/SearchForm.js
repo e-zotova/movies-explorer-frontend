ee89df3f38d47fefcd5002c0a59b961abd37edf5
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-07-09 11:33:55 +0400

    feat: searchQuery, checkbox state

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -3,17 +3,29 @@ import FilterCheckbox from "../FilterCheckbox/FilterCheckbox.js";
 
 function SearchForm({ onGetMovies }) {
   const [isFormEmpty, setIsFormEmpty] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isShortChecked, setIsShortChecked] = useState(false);
 
-  function handleSubmit(e) {
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleShortCheckbox = () => {
+    setIsShortChecked(!isShortChecked);
+  }
+
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (e.target.querySelector('input').value.length === 0) {
+    if (searchQuery.length === 0) {
       setIsFormEmpty(true);
     } else {
       setIsFormEmpty(false);
-      onGetMovies();
+      onGetMovies(searchQuery);
+      localStorage.setItem("searchQuery", searchQuery);
+      localStorage.setItem("shortMovies", isShortChecked)
     }
-  }
+  };
 
   return (
     <section className="search-form">
@@ -21,16 +33,27 @@ function SearchForm({ onGetMovies }) {
         <div className="search-form__icon"></div>
         <input
           type="text"
+          name="search-query"
           placeholder="Фильм"
+          value={searchQuery || ""}
+          onChange={handleChange}
           className="search-form__input"
         />
         <button type="submit" className="button search-form__button"></button>
-        <FilterCheckbox className="filter-checkbox_browser" />
+        <FilterCheckbox
+          className="filter-checkbox_browser"
+          isChecked={isShortChecked}
+          handleShortCheckbox={handleShortCheckbox}
+        />
       </form>
       <span className="search-form__error">
         {isFormEmpty ? "Нужно ввести ключевое слово" : ""}
       </span>
-      <FilterCheckbox className="filter-checkbox_mobile" />
+      <FilterCheckbox
+        className="filter-checkbox_mobile"
+        isChecked={isShortChecked}
+        handleShortCheckbox={handleShortCheckbox}
+      />
     </section>
   );
 }
